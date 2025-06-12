79a07b8cafa79360d904052b1c45a595886ed1ac
LevaMakeGames <levamakegames@gmail.com>
2025-06-12 21:46:05 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ import ShopScene from './scenes/ShopScene.js';
 import CardsScene from './scenes/CardsScene.js';
 import BattleScene from './scenes/BattleScene.js';
 import HelpScene from './scenes/HelpScene.js';
-import BattlePhaseScene from './scenes/BattlePhaseScene.js';
 
 Telegram.WebApp.ready();
 Telegram.WebApp.expand();
@@ -18,10 +17,8 @@ const config = {
     ShopScene,
     CardsScene,
     BattleScene,
-    BattlePhaseScene,
     HelpScene
   ]
 };
 
 new Phaser.Game(config);
-
